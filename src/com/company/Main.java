3cd8ce1f28d1e49cc0b994b1c3c878c1ca5c9e96
sanjanenic <sanjanenicbl@gmail.com>
2022-03-03 13:34:47 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       
+            int Fiction=1;
+            int NonFiction=2;
+            boolean discount;
+
+            if (NonFiction>2 && Fiction==0){
+                discount = true;
+                System.out.println("I have a discount");
+            }
+
+            else {
+                discount= false;
+                System.out.println("I have not a discount");
+
+
+
+
+        }
+
+        public static boolean
+    }
+}
