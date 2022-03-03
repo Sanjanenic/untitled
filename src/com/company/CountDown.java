7c53d937e4db0cb0efe58285f0f8fa748f330129
sanjanenic <sanjanenicbl@gmail.com>
2022-03-03 15:44:05 +0100

    funktioniert jetzt

--- src/com/company/CountDown.java
+++ src/com/company/CountDown.java
@@ -4,15 +4,15 @@ public class CountDown {
     public static void main(String[] args) {
 
         int first;
-        int last;
-        CountDownVon(10);
+        //int last;Ovo mi ne treba
+        CountDownVon(19);
 
 
     }
 
     public static void CountDownVon(int first){
 
-         int last =0;
+         //int last =0;
          while (first!=0){
              System.out.println (first);
              first --;
