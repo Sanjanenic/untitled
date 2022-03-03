4b99799187ed5fa6f139cf37f1015b19b939c518
sanjanenic <sanjanenicbl@gmail.com>
2022-03-03 15:11:53 +0100

    swich

--- src/com/company/SwitshBeispiel.java
+++ src/com/company/SwitshBeispiel.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class SwitshBeispiel {
     public static void main(String[] args) {
-
+     System.out.println (getGradeText(2));
 
     }
 
@@ -10,17 +10,28 @@ public class SwitshBeispiel {
         String grateText;
         return switch(grade){
            case 1 -> grateText = "Outstanding!";
-           case 2 -> grateText = "Exellent!";
+           case 2 -> grateText = "Excellent!";
            case 3 -> grateText = "Acceptable!";
            case 4 -> grateText = "Passing!";
            case 5 -> grateText = "Fail!";
            default -> grateText = null;
 
-
         };
 
 
 
+
+    }
+    public static int getGrade(String gradeText){
+        return switch ((gradeText)){
+            case "Outstanding!" -> 1;
+            case "Exellent!!" -> 2;
+            case "Acceptable!" -> 3;
+            case "Passing!" -> 4;
+            case "Fail!" -> 5;
+            default -> 0;
+
+        };
 
     }
 }
